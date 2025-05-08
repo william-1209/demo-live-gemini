12a1ea14adbcc27a520a7bcb0e1639d002f97cc2
Mai Ly <mailypd02033@gmail.com>
2025-05-08 12:01:21 +0700

    update gitignore

--- src/components/side-panel/SidePanel.tsx
+++ src/components/side-panel/SidePanel.tsx
@@ -76,7 +76,7 @@ export default function SidePanel() {
   return (
     <div className={`side-panel ${open ? "open" : ""}`}>
       <header className="top">
-        <h2>Console</h2>
+        <h2>Demo live</h2>
         {open ? (
           <button className="opener" onClick={() => setOpen(false)}>
             <RiSidebarFoldLine color="#b4b8bb" />
